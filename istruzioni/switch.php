58bbe496260e55f23686619d84f4ce1e4cbbbc5d
ginlemongin <davide.morolli@enaipriminifad.org>
2023-01-12 17:29:23 +0100

    esercizio compleanno

--- istruzioni/switch.php
+++ istruzioni/switch.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
 <html>
     <head>
         <meta charset="utf-8">
@@ -29,7 +29,7 @@ $m=2;
 //quanti giorni mancano alla fine del mese
 /*funzione DATE*/
 
-echo date("d/m/Y");
+/*echo date("d/m/Y");
 $m= date("m");
 switch($m){
     case 2:
@@ -43,7 +43,7 @@ switch($m){
         break;
     default:
         $giorni_del_mese = 31;
-}
+}*/
 //contiene il giorno di oggi
 //$g = date("d");
 $giorni_mancanti = $giorni_del_mese - date("d");
@@ -51,6 +51,25 @@ echo "<br>giorni mancanti = $giorni_mancanti";
 
 //esercizio
 /*mostra quanti giorni mancano al tuo compleanno, prima valuto se è lo stesso mese,somma di ogni mese successivo quanti giorni mi servono*/
+date("d/m/Y");
+$m= date("m");
+switch($m){
+    case 2:
+        $giorni_del_mese = 28;
+        break;
+    case 4:
+        $giorni_del_mese = 30;
+    case 6:
+        $giorni_del_mese = 30;
+    case 9:
+        $giorni_del_mese = 30;
+    case 11:
+        $giorni_del_mese = 30;
+        break;
+    default:
+        $giorni_del_mese = 31;
+}
+
       ?> 
     </body>
 </html>
