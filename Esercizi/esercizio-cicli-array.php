0b80313f412403c8f5240ef8d1a52f06f8f698fd
ginlemongin <davide.morolli@enaipriminifad.org>
2023-01-15 19:39:45 +0100

    esercizio array

---
+++ Esercizi/esercizio-cicli-array.php
@@ -0,0 +1,11 @@
+<!-- Esercizio PHP
+
+Viene fornito un array di interi, dove ogni numero rappresenta l&#39;età degli abitanti di un piccolo
+comune. Si chiede di svolgere le seguenti attività:
+1. elencare tutte le età presenti nell&#39;array con uno sfondo alternato nelle varie righe,
+scrivendo in grassetto le età dei maggiorenni e in corsivo quelle dei minorenni (0
+corrisponde ad un neonato);
+2. in un riquadro, riportare il numero dei neonati e l&#39;età del più anziano;
+3. calcolare e visualizzare l&#39;età media dei maggiorenni.
+4. Per aver diritto al voto si deve avere almeno 18 anni: calcolare e visualizzare quante
+schede servono in totale per le elezioni. -->
