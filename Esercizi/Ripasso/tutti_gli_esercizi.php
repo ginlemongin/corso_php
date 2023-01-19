84c8bab6266e7f85b942dff99ee758cf3537db9a
ginlemongin <davide.morolli@enaipriminifad.org>
2023-01-19 12:51:22 +0100

    esercizi ripoasso verifica

---
+++ Esercizi/Ripasso/tutti_gli_esercizi.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // <!-- Dati $a e $b, calcolare la media tra i 2 numeri -->
+    // $a=5;
+    // $b=2;
+        
+    //         echo ($a + $b) / 2;
+
+
+
+    // Scrivere se la variabile $x è multipla di 3
+    // $x=5;
+    
+    // if  (($x % 3)==0) {
+        
+    //     echo "multiplo";
+    // }else{
+    //     echo "non multiplo";
+    // }
+
+
+    //Invertire il contenuto di $x e $y
+        
+    ?>
+</body>
+</html>
