e0708056072ecf2fa1dd9701d5949db54d709b8d
ginlemongin <davide.morolli@enaipriminifad.org>
2023-02-13 14:20:39 +0100

    esercizio completo cicili

--- Esercizi/Ripasso/Esercizio_completo_cicli.php
+++ Esercizi/Ripasso/Esercizio_completo_cicli.php
@@ -14,19 +14,19 @@
 
 1. Realizzare una pagina php che presenti una sequenza di numeri interi con sfondo alternato per
 ogni riga. I dati elencati devono rispettare le seguenti regole:
- la sequenza deve iniziare da un numero assegnato (memorizzato nella variabile $start) e
+- la sequenza deve iniziare da un numero assegnato (memorizzato nella variabile $start) e
 analizzare tutti gli interi precedenti;
- l'analisi deve essere ripetuta per un numero di volte assegnato (memorizzato nella variabile
+- l'analisi deve essere ripetuta per un numero di volte assegnato (memorizzato nella variabile
 $repeat);
- se il numero considerato corrisponde al mese della data corrente, deve essere scritto in
+- se il numero considerato corrisponde al mese della data corrente, deve essere scritto in
 corsivo;
- di ogni numero considerato, scrivere sulla stessa riga il suo valore e il valore doppio (in
+- di ogni numero considerato, scrivere sulla stessa riga il suo valore e il valore doppio (in
 rosso)
-2. Al termine dell&#39;elenco, separando con una riga orizzontale, scrivere:
- i dati assegnati, con questa specifica frase: “$start: ''; $repeat: ''; “;
- quanti numeri pari sono stati scritti (considerando anche i valori del doppio);
- il numero di ripetizioni effettivamente eseguite.
-3. Modificare l&#39;algoritmo che produce la lista del punto 1, inserendo la condizione che non si
+2. Al termine dell' elenco, separando con una riga orizzontale, scrivere:
+- i dati assegnati, con questa specifica frase: “$start: ''; $repeat: ''; “;
+- quanti numeri pari sono stati scritti (considerando anche i valori del doppio);
+- il numero di ripetizioni effettivamente eseguite.
+3. Modificare l'algoritmo che produce la lista del punto 1, inserendo la condizione che non si
 devono considerare i numeri negativi.
 FACOLTATIVO: realizzare una seconda pagina identica alla precedente con una istruzione ciclica
 differente */
@@ -34,17 +34,26 @@ differente */
     $start = 20;
     $repeat = 2;
 
-for ($repeat;  $repeat >=1;  $repeat--) {
-    
-    for ($start; $start >= 0; $start--) {
-        if ($start % 2) {
-            echo "<p style=background-color:#ccc>";
-            echo "$start<br>";
-        }else {
-            echo "<p style=background-color:#fff>";
-            echo "$start<br>";
-        }
-        
+    for ($repeat; $repeat >= 1; $repeat--) {
+
+        for ($i = $start; $i >= 0; $i--) {
+            $double=$i*2;
+
+             if ($i % 2) {
+                if (date("m")==$i) {
+                    echo "<p style=background-color:#ccc><i>$i</i> $double</p>";
+                }else {
+                    echo "<p style=background-color:#ccc>$i $double</p>";
+                }
+                 
+             } else {
+                if (date("m")==$i) {
+                    echo "<p style=background-color:#fff><i>$i</i> $double</p>";
+                }else {
+                    echo "<p style=background-color:#fff>$i $double</p>";
+                }
+             }
+           
     }
 }
     ?>
