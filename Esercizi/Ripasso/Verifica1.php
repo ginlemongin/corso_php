e336a92d459fdab2d3a510d8bca9f44afb886dc1
ginlemongin <davide.morolli@enaipriminifad.org>
2023-02-14 15:04:44 +0100

    ripasso verifica1

--- Esercizi/Ripasso/Verifica1.php
+++ Esercizi/Ripasso/Verifica1.php
@@ -25,7 +25,82 @@ quante volte è stato assegnato il punteggio più basso (tra quelli assegnati re
 se il punteggio ottenuto è maggiore di 8: calcolare e visualizzare quanti sono i partecipanti
 che potrebbero essere immediatamente assunti e di questi la percentuale che rimane in lista
 d&#39;attesa. */
-    ?>
-</body>
 
+
+    //numero partecipanti random meno 5 malati e le lodi
+    $numero_partecipanti = rand(10, 100) - 5;
+    $lodi = 0;
+    //la variabile che memorizza il voto minimo deve partire dal massimo a scalare
+    $voto_min = 10;
+    //variabili che contano la somma dei voti insufficienti ed il numero dei bocciati
+    $punteggio_medio = 0;
+    $bocciati = 0;
+    //posti di lavoro disponibili
+    $posti = 5;
+    $assumibili = 0;
+
+    //aggiungo la unordered list
+    "<ul>";
+    //inizializzo array voti di ogni partecipante random
+    for ($i = 0; $i < $numero_partecipanti; $i++) {
+        $voti[$i] = rand(0, 10);
+
+        //conto i voti 10
+        if ($voti[$i] == 10) {
+            $lodi++;
+        }
+
+        //conto i voti piu bassi
+        if ($voti[$i] < $voto_min) {
+            $voto_min = $voti[$i];
+            $totale_voti_minimi = 0;
+        }
+        if ($voti[$i] == $voto_min) {
+            $totale_voti_minimi++;
+        }
+
+        //calcolo punteggio medio bocciati
+        if ($voti[$i] < 6) {
+            $punteggio_medio += $voti[$i];
+            $bocciati++;
+        }
+
+        //conto le persone assumbili subito
+        if ($voti[$i] > 8) {
+            $assumibili++;
+        }
+
+        //imposto lo sfondo di ogni riga e lo stile del voto.
+        //gestisco lo zero
+        if (($i % 2) && ($voti[$i] == 0)) {
+            echo "<p><li><i>$voti[$i]</i></li></p>";
+        } elseif (($i % 2 == 0) && ($voti[$i] == 0)) {
+            echo "<p><li style=background-color:gray><i>$voti[$i]</i></li></p>";
+        } elseif (($i % 2) && ($voti[$i] >= 6)) {
+            echo "<p><li><b>$voti[$i]</b></li></p>";
+        } elseif (($i % 2) && ($voti[$i] < 6)) {
+            echo "<p><li><i>$voti[$i]</i></li></p>";
+        } elseif ($i >= 6) {
+            echo "<p><li style=background-color:gray><b>$voti[$i]</b></li></p>";
+        } else {
+            echo "<p><li style=background-color:gray><i>$voti[$i]</i></li></p>";
+        }
+    }
+    "</ul>";
+
+    echo "<fieldset> <p>Partecipanti promossi a pieni voti: $lodi</p>
+        <p>Voti minimi: $totale_voti_minimi</p></fieldset>";
+    echo "Media voti bocciati: " . $punteggio_medio / $bocciati . "<br>";
+
+    //stampa delel assunzioni e liste di attesa
+    if ($assumibili > $posti) {
+        //stampa solo se gli assumbili sono più dei posti disponibili
+        echo "Assunti subito: " . $posti . "<br>";
+        echo "Le persone in lista d'attesa sono: " . $assumibili-$posti;
+    } else {
+        echo "Assunti subito: " . $posti;
+    }
+    ?>
+
+</body>
 </html>
